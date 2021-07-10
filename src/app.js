60ea18fb7c52cd3756976dbf94d520b8cab48a2b
ConnieBerrios <connieberrios@live.com>
2021-07-10 08:08:01 +0000

    extentions

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+var pronoun = ["the","our"];
+var adj = ["great","big"];
+var noun = ["jogger","racoon"];
+var ext = [".com",".net",".us",".io"]
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+for (let i = 0; i < pronoun.length; i++) 
+  for (let j = 0; j <adj.length; j++) 
+    for (let n = 0; n <noun.length; n++) 
+        for (let e = 0; e<ext.length; e++) 
+      console.log(pronoun[i],adj[j],noun[n],ext[e]);
+    
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
